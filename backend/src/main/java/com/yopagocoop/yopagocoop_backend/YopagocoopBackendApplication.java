2b5bba0ab045d191cdc72aed0b60c2a69d1d1778
s0-dev <117926328+s0-dev@users.noreply.github.com>
2025-03-20 16:08:39 -0300

    feat (springboot): Added template from Spring initializr

---
+++ backend/src/main/java/com/yopagocoop/yopagocoop_backend/YopagocoopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yopagocoop.yopagocoop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YopagocoopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YopagocoopBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yopagocoop/yopagocoop_backend/YopagocoopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yopagocoop.yopagocoop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YopagocoopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
